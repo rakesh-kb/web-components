63dba2421d4c141ebfc79f698386d08ef6263469
Rakesh KB <rakesh.kb@volvocars.com>
2023-06-28 16:34:02 +0530

    Adding shadow DOM

--- tooltip.js
+++ tooltip.js
@@ -1,35 +1,41 @@
 class Tooltip extends HTMLElement {
-    constructor() {
-        // Fixing error: Uncaught ReferenceError: Must call super constructor in derived class before accessing 'this' or returning from derived constructor at new Tooltip
-        super();
-        this._tooltipContainer;
-        this._tooltipText = 'This is the tooltip text placeholder';
-    }
-    
-    connectedCallback() {
-        if (this.hasAttribute('text')) {
-            this._tooltipText = this.getAttribute('text');
-        }
-        const tooltipIcon = document.createElement('span');
-        tooltipIcon.textContent = ' (?)';
-        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
-        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-        this.appendChild(tooltipIcon);
-    }
+  constructor() {
+    // Fixing error: Uncaught ReferenceError: Must call super constructor in derived class before accessing 'this' or returning from derived constructor at new Tooltip
+    super();
+    this._tooltipContainer;
+    this._tooltipText = "This is the tooltip text placeholder.";
+    // creating a shadow DOM
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+        <slot>Default text</slot>
+        <span> (?) </span>
+    `;
+  }
 
-    // pseudo private method
-    _showTooltip() {
-        this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = this._tooltipText;
-        this.appendChild(this._tooltipContainer);
+  connectedCallback() {
+    if (this.hasAttribute("text")) {
+      this._tooltipText = this.getAttribute("text");
     }
+    const tooltipIcon = document.createElement("span");
+    tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
+    tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = 'relative';
+  }
 
-    _hideTooltip() {
-        this.removeChild(this._tooltipContainer);
-    }
+  // pseudo private method
+  _showTooltip() {
+    this._tooltipContainer = document.createElement("div");
+    this._tooltipContainer.textContent = this._tooltipText;
+    this.shadowRoot.appendChild(this._tooltipContainer);
+  }
 
-    attributeChangedCallback() {}
-    disconnectedCallback() {}
+  _hideTooltip() {
+    this.shadowRoot.removeChild(this._tooltipContainer);
+  }
+
+  attributeChangedCallback() {}
+  disconnectedCallback() {}
 }
 
-customElements.define('kb-tooltip', Tooltip);
+customElements.define("kb-tooltip", Tooltip);
